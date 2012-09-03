0b80368c694773549172a2cb640f8dd4997f9038
Daniel Phillimore <dan@ovms.co>
2012-09-03 14:57:58 +0100

    Large refactoring to accommodate prototype support

--- js/accessorIE.js
+++ js/accessorIE.js
@@ -21,14 +21,32 @@
 (function () {
     "use strict";
 
-    var global = new [Function][0]("return this;")(),
+    var hasOwnProperty = {}.hasOwnProperty,
+        global = new [Function][0]("return this;")(),
         Object = global.Object,
+        Array = global.Array,
         document = global.document,
         sandbox = null,
+        objects = [],
         needsAccessorShim = !!global.execScript,
         namespacePath = null,
         namespaceName = "accessorIE",
-        transportName = "__transport__";
+        transportName = "__transport__",
+        info = {
+            array: {
+                names: [],
+                lookup: {}
+            }
+        },
+        hostPropertyNames = [
+            "nextSibling", "childNodes", "canHaveHTML", "recordNumber", "previousSibling", "nodeName",
+            "currentStyle", "clientHeight", "style", "nodeType", "scopeName", "offsetWidth", "filters",
+            "isContentEditable", "scrollHeight", "lastChild", "canHaveChildren", "isMultiLine",
+            "offsetTop", "parentNode", "tagName", "behaviorUrns", "parentTextEdit", "ownerDocument",
+            "offsetParent", "parentElement", "children", "readyState", "document", "firstChild",
+            "sourceIndex", "isTextEdit", "isDisabled", "runtimeStyle", "scrollWidth", "attributes",
+            "offsetHeight", "clientTop", "clientWidth", "tagUrn", "clientLeft", "all", "offsetLeft"
+        ];
 
     each(document.getElementsByTagName("script"), function () {
         var path = this.src,
@@ -47,6 +65,55 @@
         throw new Error("accessorIE script not found for path extraction");
     }
 
+    each([
+        "shift", "sort", "concat", "filter",
+        "map", "join", "toString", "push",
+        "reduceRight", "toLocaleString", "constructor", "forEach",
+        "lastIndexOf", "some", "slice", "every",
+        "reverse", "indexOf", "unshift",
+        "reduce", "pop", "splice"
+    ], function (name) {
+        if (Array.prototype.hasOwnProperty(name)) {
+            info.array.names.push(name);
+            info.array.lookup[name] = true;
+        }
+    });
+
+    if (!global.console) {
+        global.console = {
+            error: function () {},
+            log: function () {}
+        };
+    }
+
+    if (!Array.prototype.indexOf) {
+        Array.prototype.indexOf = function (value) {
+            var index,
+                length = this.length;
+
+            for (index = 0; index < length; index += 1) {
+                if (this[index] === value) {
+                    return index;
+                }
+            }
+
+            return -1;
+        };
+    }
+
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function (callback, thisObj) {
+            var index,
+                length = this.length;
+
+            thisObj = thisObj || this;
+
+            for (index = 0; index < length; index += 1) {
+                callback.call(thisObj, this[index], index, this);
+            }
+        };
+    }
+
     if (!Object.keys) {
         Object.keys = function (obj) {
             var keys = [];
@@ -57,6 +124,16 @@
 
             return keys;
         };
+    }
+
+    if (!global.TypeError) {
+        (function () {
+            function TypeError(msg) {
+                Error.call(msg);
+            }
+            TypeError.prototype = Object.create(Error.prototype);
+            global.TypeError = TypeError;
+        }());
     }
 
     if (!Object.create) {
@@ -70,7 +147,15 @@
             setupComponent();
 
             Object.defineProperty = function (obj, name, descriptor) {
-                obj[transportName].defineProperty(name, descriptor);
+                if (obj[transportName]) {
+                    obj[transportName].defineProperty(name, descriptor);
+                } else {
+                    if (!hasOwnProperty.call(descriptor, "value")) {
+                        throw new Error("Object.defineProperty() :: Only data descriptors supported on JScript objects");
+                    }
+
+                    obj[name] = descriptor.value;
+                }
 
                 return obj;
             };
@@ -78,25 +163,65 @@
             Object.defineProperties = function (obj, descriptors) {
                 each(descriptors, function (descriptor, name) {
                     Object.defineProperty(obj, name, descriptor);
-                });
+                }, { keys: true });
 
                 return obj;
             };
 
             Object.getOwnPropertyDescriptor = function (obj, name) {
-                return obj[transportName].getOwnPropertyDescriptor(name);
+                if (obj[transportName]) {
+                    return obj[transportName].getOwnPropertyDescriptor(name);
+                }
+
+                return {
+                    configurable: true,
+                    enumerable: true,
+                    value: obj[name],
+                    writable: true
+                };
             };
 
-            Object.getPrototypeOf = function (obj) {
-                return obj.__proto__;
+            Object.getOwnPropertyNames = function (obj) {
+                var name,
+                    names,
+                    lookup;
+
+                if (typeof obj !== "object") {
+                    throw new TypeError("Object.getOwnPropertyNames called on non-object");
+                }
+
+                if (obj === Array.prototype) {
+                    names = info.array.names.slice();
+                    lookup = Object.create(info.array.lookup);
+                } else {
+                    names = [];
+                    lookup = {};
+                }
+
+                for (name in obj) {
+                    if (hasOwnProperty.call(obj, name) && !lookup[name]) {
+                        names.push(name);
+                        lookup[name] = true;
+                    }
+                }
+
+                if (obj[transportName]) {
+                    names = names.concat(obj[transportName].getOwnPropertyNames(lookup));
+                }
+
+                return names;
             };
 
             Object.create = (function (parent) {
                 return function (extend, propertyDescriptors) {
                     var obj;
 
-                    if (extend === null) {
+                    if (extend === null || extend === Array.prototype) {
                         obj = createObject();
+
+                        if (extend) {
+                            obj.__proto__ = extend;
+                        }
 
                         if (propertyDescriptors) {
                             Object.defineProperties(obj, propertyDescriptors);
@@ -151,20 +276,48 @@
             "<head>" +
                 "<?import namespace='" + namespaceName + "' />" +
                 "<script>document.namespaces['" + namespaceName + "'].doImport('" + namespacePath + "');</script>" +
-            "</head>"
+            "</head><body></body>"
         );
         sandbox.close();
+
+        function cleanUp() {
+            global.detachEvent("onbeforeunload", cleanUp);
+
+            /*each(objects, function (object) {
+                each(hostPropertyNames, function (name) {
+                    object[transportName].deleteProperty(name);
+                });
+                //sandbox.body.removeChild(object);
+            });*/
+            objects.length = 0;
+            sandbox = null;
+            CollectGarbage();
+        }
+
+        global.attachEvent("onunload", cleanUp);
     }
 
     function createObject() {
-        var obj = sandbox.createElement(namespaceName + ":object");
+        var obj = sandbox.createElement(namespaceName + ":object"),
+            transport;
 
-        sandbox.appendChild(obj);
+        sandbox.body.appendChild(obj);
+        //sandbox.body.removeChild(obj); // Might cause problems...
 
-        if (!obj[transportName]) {
+        objects.push(obj);
+
+        transport = obj[transportName];
+
+        if (!transport) {
             throw new Error("accessorIE shim .htc missing or served with wrong MIME type");
         }
 
-        return obj;
+        each(hostPropertyNames, function (name) {
+            Object.defineProperty(obj, name, {
+                value: undefined
+            });
+        });
+
+        return transport.init(global);
     }
 }());
