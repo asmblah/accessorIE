c65d93126887851a2576b1d7a51c4159b5ba9e76
Daniel Phillimore <dan@ovms.co>
2012-09-03 17:16:23 +0100

    Added some missing tests for ES5 behaviour

--- tests/bdd/ObjectTest.js
+++ tests/bdd/ObjectTest.js
@@ -46,14 +46,70 @@ define([
 
                 expect(obj.prop).to.equal(9);
             });
+
+            describe("for a data descriptor", function () {
+                it("should default 'configurable' data descriptor attribute to false", function () {
+                    Object.defineProperty(obj, "parent", {
+                        value: "guardian"
+                    });
+
+                    expect(Object.getOwnPropertyDescriptor(obj, "parent").configurable).to.equal(false);
+                });
+
+                it("should default 'enumerable' data descriptor attribute to false", function () {
+                    Object.defineProperty(obj, "parent", {
+                        value: "guardian"
+                    });
+
+                    expect(Object.getOwnPropertyDescriptor(obj, "parent").enumerable).to.equal(false);
+                });
+
+                it("should default 'writable' data descriptor attribute to false", function () {
+                    Object.defineProperty(obj, "parent", {
+                        value: "guardian"
+                    });
+
+                    expect(Object.getOwnPropertyDescriptor(obj, "parent").writable).to.equal(false);
+                });
+            });
         });
 
         describe("defineProperties", function () {
+            it("should not error when given an empty hash of descriptors", function () {
+                Object.defineProperties(obj, {});
+            });
 
+            it("should be able to define a single property", function () {
+                var descriptor = {
+                    value: "Frank",
+                    writable: true,
+                    enumerable: true,
+                    configurable: true
+                };
+
+                Object.defineProperties(obj, {
+                    name: descriptor
+                });
+
+                expect(Object.getOwnPropertyDescriptor(obj, "name")).to.eql(descriptor);
+            });
         });
 
         describe("getOwnPropertyDescriptor", function () {
+            describe("for a data descriptor", function () {
+                it("should return all relevant descriptor attributes", function () {
+                    Object.defineProperty(obj, "parent", {
+                        value: "guardian"
+                    });
 
+                    expect(Object.getOwnPropertyDescriptor(obj, "parent")).to.eql({
+                        configurable: false,
+                        enumerable: false,
+                        value: "guardian",
+                        writable: false
+                    });
+                });
+            });
         });
 
         describe("getOwnPropertyNames", function () {
